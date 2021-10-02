6d583adab8c7e234c24d7844b1d03fbf449f815a
jose aquafonte <jmfls@iscte.pt>
2021-10-02 14:33:20 +0100

    hello?

--- main.py
+++ main.py
@@ -9,5 +9,6 @@ from Thing import runRobot
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     runRobot()
+    #teste para o github
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
